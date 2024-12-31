3642f92df47c52b5cf2f068fcbb1fb3b93af62ca
KW1610 <kanatturusbaev9@gmail.com>
2024-12-31 14:56:08 +0600

    All

---
+++ about.php
@@ -0,0 +1,240 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>RPCS3 - About</title>
+<meta charset="utf-8">
+<meta name="description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, about">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - About"/>
+<meta property="og:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering."/>
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png"/>
+<meta property="og:image:width" content="1200"/>
+<meta property="og:image:height" content="630"/>
+<meta property="og:url" content="https://rpcs3.net"/>
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website"/>
+<meta property="og:site_name" content="RPCS3"/>
+<meta name="twitter:title" content="RPCS3 - About">
+<meta name="twitter:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header" style="background: url('/img/BreadForMainPageBack.jpg');">
+
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>О проекте</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Узнайте больше о нашем проекте и с нашими участниками.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Проект</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>Наш проект, названный “Нан учун Гана”, является важной инициативой, которую предприняла команда Enactus KEU. Эта инициатива направлена на сбор и переработку хлебных отходов с целью производства корма для скота. Это не только помогает уменьшить количество отходов, но и способствует созданию устойчивого источника питания для животных.
+
+В рамках этого проекта было установлено более 400 бочек в городах Бишкек и Кант. Эти бочки служат местами для сбора хлебных отходов от местных жителей. Это позволяет каждому жителю внести свой вклад в улучшение окружающей среды и устойчивое развитие своего города.
+
+Собранные хлебные отходы затем подвергаются процессу переработки с использованием современного оборудования и технологий. Этот процесс был тщательно разработан и оптимизирован для превращения хлебных отходов в высококачественный корм для животных. Это не только улучшает качество корма, но и значительно сокращает количество отходов, которые обычно окажутся на свалке.
+</p>
+
+								</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right conus'>
+					<div class='splitter-img-wrapper' style="background: url('/img/projectAboutPage.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+					<div class='splitter-img-container splitter-con-left'>
+						<div class='splitter-img-wrapper' style="background: url('/img/fodderAboutPage.jpg') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<div class='splitter-txt-wrapper'>
+						<div class='container-con-block darkmode-block'>
+							<div class='container-con-wrapper'>
+								<div class='container-tx1-block darkmode-txt'>
+									<div class='container-emp-block'>
+									</div>
+									<h2>Корм и его воздействие на окружение</h2>
+								</div>
+
+<div class='container-tx2-block darkmode-txt'>
+<p>Наш переработанный корм для скота играет ключевую роль в экономии средств фермеров на кормление, что приводит к значительной экономии в их бюджетах. Помимо этого, его доступная цена - 30 сом за килограмм, с уменьшением стоимости до 25 сом за килограмм при покупке 100 килограмм и до 20 сом за килограмм при покупке 1000 килограмм - делает его еще более привлекательным для использования. Благодаря содержанию высококачественных питательных веществ, наш корм способствует не только улучшению здоровья, но и повышению производительности скота, что в конечном итоге обеспечивает более успешное и прибыльное сельское хозяйство. Кроме того, наш процесс переработки позволяет более эффективно использовать ограниченные ресурсы, так как он уменьшает потребление земельных и водных ресурсов, сокращает пищевые отходы и создает замкнутый цикл использования ресурсов. Таким образом, наша инициатива имеет не только экономическое, но и экологическое значение, помогая сохранить баланс в экосистеме суши и обеспечить устойчивое развитие сельского хозяйства.	</p>
+</div>
+
+							</div>
+						</div>
+					</div>
+					<div class='splitter-img-container-spec splitter-con-left'>
+						<div class='splitter-img-wrapper' style="background: url('/img/fodderAboutPage.jpg') no-repeat center; background-size: cover;">
+						</div>
+					</div>
+					<style>
+
+						</style>
+				</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Какие проблемы ЦУР решает наш проект?</h2>
+							</div>
+
+
+							<div class='container-tx2-block darkmode-txt'>
+    <p>Наш проект перерабатывает выброшенный хлеб в корм для животных, решая несколько важных проблем:</p>
+    <ul>
+        <li><strong>Нищета:</strong> Сокращаем расходы фермеров на кормление скота, улучшаем их финансовое положение.</li>
+        <li><strong>Голод:</strong> Повышаем доступность качественного корма для скота, особенно для фермеров с ограниченными ресурсами.</li>
+        <li><strong>Здоровье и благополучие:</strong> Улучшаем здоровье и производительность скота, обеспечивая необходимые питательные вещества.</li>
+        <li><strong>Ответственное потребление и производство:</strong> Сокращаем количество пищевых отходов, создаем замкнутый цикл использования ресурсов.</li>
+        <li><strong>Сохранение экосистем суши:</strong> Уменьшаем потребление земельных и водных ресурсов, используя пищевые отходы вместо выращивания кормовых культур.</li>
+    </ul>
+</div>
+
+
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right conus'>
+					<div class='splitter-img-wrapper' style="background: url('/img/problems.png') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			
+
+			
+			<!-- <div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Участники</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									
+В настоящее время проект "Нан үчүн гана" поддерживается и организуется уникальным составом участников, каждый из которых играет важную роль в различных аспектах проекта. Будь то активное участие в переработке выброшенной еды в корм для животных, координация усилий сообщества или даже обновление и поддержание данного веб-сайта в актуальном состоянии - каждый вносит свой вклад, который дополняет общую картину и помогает достичь наших целей. <br>
+									<br>
+									Внутренние члены команды - это лишь часть участников проекта. Мы также ценим вклад многих разработчиков, которые помогают нам в развитии и улучшении процесса переработки еды и её превращения в корм для животных. Хотя многие из них могут приходить и уходить, мы приветствуем всех, кто заинтересован в нашей инициативе и готов внести свой вклад в улучшение нашего проекта.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/aboutPageTeam.jpeg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div> -->
+
+<!-- -----------------------     Основатели      ---------------------------- -->
+
+
+			<!-- <div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>Основатели</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							Список ключевых лиц в основании проекта.
+						</p>
+					</div>
+				</div>
+			</div> -->
+
+<!-- ------------------------------------------------------------------------------ -->
+
+			<!-- <?php include 'lib/module/about/inc-about-founders.php';?> -->
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2 id='participants'>Участники</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+							Люди, работающие не поклодая рук, над этим проектом.
+						</p>
+					</div>
+				</div>
+			</div>
+			<?php include 'lib/module/about/inc-about-current.php';?>
+			<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<div class='container-emp-block'>
+						</div>
+						<h2>Инвесторы и бенефициары</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p>
+						Люди, которые поддержали наш проект финансово.
+						</p>
+					</div>
+				</div>
+			</div>
+			<?php include 'lib/module/about/inc-about-former.php';?>
+		</div>
+	</div>
+	<style>
+		@media (max-width:800px){
+			.conus{
+				display:block;
+			}
+		}
+		@media (min-width:800px){
+			.splitter-img-container-spec{
+				display:none;
+			}
+		}
+	</style>
+</div>
+</div>
+<?php include 'lib/module/inc-footer.php';?>
+</div>
+</body>
+</html>
---
+++ lib/module/home/inc-home-carousel.php
@@ -0,0 +1,98 @@
+<div class="container fill">
+	<div id="myCarousel" class="carousel slide">
+		<div class="carousel-inner">
+			<!-- Carousel Slide -->
+			<div class="item active">
+				<div class="fill">
+					<div class="video-con-container">
+						<!-- -- Видео --  -->
+						<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<!-- <div class="video-btn-play">
+								</div> -->
+								<!-- <div class="video-ico-service">
+								</div> -->
+								<div class='video-con-animate page-video-1'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/photocarusel2.jpg') no-repeat center; background-size: cover;">
+								<!-- <div class="video-img-overlay">
+								</div> -->
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-4">
+								<div class='video-emp-block'>
+								</div>
+								<h2>Низкая стоимость корма</h2>
+								<h3>Покупая до 100кг стоимость корма 30сом
+									<br>Покупая от 100кг до 1т стоимость 25сом
+									<br>Покупая от 1т стоимость 20сом
+								</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+					<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								<!-- <div class="video-btn-play">
+								</div> -->
+								<!-- <div class="video-ico-service">
+								</div> -->
+								<div class='video-con-animate page-video-1'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/photocarusel1.jpg') no-repeat center; background-size: cover;">
+								<!-- <div class="video-img-overlay">
+								</div> -->
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-5">
+								<div class='video-emp-block'>
+								</div>
+								<h2>Здоровое питание</h2>
+								<h3>Наш корм сделан из хлеба, что делает наш корм идеальным и сбалонсированным питанием для домашних животных</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel Slide -->
+			<!-- <div class="item">
+				<div class="fill">
+					<div class="video-con-container">
+					<div class='video-con-left'>
+							<div class='video-con-wrapper'>
+								 <div class="video-btn-play">
+								</div> 
+								 <div class="video-ico-service">
+								</div> 
+								<div class='video-con-animate page-video-1'>
+								</div>
+								<div class='video-img-thumbnail' style="background: url('/img/carousel4.jpg') no-repeat center; background-size: cover;">
+								 <div class="video-img-overlay">
+								</div>
+								</div>
+							</div>
+						</div>
+						<div class='video-con-right'>
+							<div class="video-tx1-description darkmode-txt scale-content-txt-6">
+								<div class='video-emp-block'>
+								</div>
+								<h2>Экономическая выгода</h2>
+								<h3>Переработка отходов в корм для животных может быть экономически выгодной, помогая снизить затраты на закупку коммерческих кормов.
+</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div> -->
+		</div>
+	</div>
+</div>
